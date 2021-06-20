fb798870ddd61a9539f409a89e2fc8e528f6b744
LTA <37591675+LikeToAccess@users.noreply.github.com>
2021-06-20 00:15:29 -0500

    Started work on bot

---
+++ bot.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# filename          : bot.py
+# description       : Main bot stuff
+# author            : LikeToAccess
+# email             : liketoaccess@protonmail.com
+# date              : 06-20-2021
+# version           : v1.0
+# usage             : python bot.py
+# notes             :
+# license           : MIT
+# py version        : 3.8.2 (must run on 3.6 or higher)
+#==============================================================================
+import discord
+import youtube_dl
